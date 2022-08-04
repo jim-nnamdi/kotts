4c4953789c5aed592c42ca0c349c6a05bd4eab03
jim-nnamdi <jimsamuel50@gmail.com>
2022-08-04 03:06:55 +0100

    add extra checks to user registration

--- internal/user/user_handler.go
+++ internal/user/user_handler.go
@@ -2,7 +2,6 @@ package user
 
 import (
 	"errors"
-	"log"
 
 	"github.com/jim-nnamdi/kotts/internal/database"
 	"go.uber.org/zap"
@@ -21,11 +20,14 @@ func (usermodel *User) AddNew(username string, email string, password string, co
 		usermodel.Logger.Debug("could not run add user query successfully", zap.Error(err))
 		return false, errors.New(err.Error())
 	}
-	x, er := res.Exec(username, password, country, email, active)
-	if er != nil {
-		log.Print(er)
-		log.Print(x)
+	result_from_user_registration, err := res.Exec(username, password, country, email, active)
+	if err != nil {
+		usermodel.Logger.Debug("could not execute and insert user into database" + err.Error())
+		return false, err
 	}
-
+	check_last_data_inserted, err := result_from_user_registration.RowsAffected()
+	if check_last_data_inserted == 0 || check_last_data_inserted < 0 {
+		return false, err
+	}
 	return true, nil
 }
