d10a2c2cc3e716c1d6b127eb2c05dc614b84ec09
jim-nnamdi <jimsamuel50@gmail.com>
2022-09-07 22:02:41 +0100

    add models to insurance directories

--- internal/insurance/insurancemodel.go
+++ internal/insurance/insurancemodel.go
@@ -1,1 +1,33 @@
 package insurance
+
+import "time"
+
+type MobileInsurance struct {
+	Id           int       `json:"id"`
+	Name         string    `json:"name"`
+	Email        string    `json:"email"`
+	Phonenumber  string    `json:"phonenumber"`
+	Nameofphone  string    `json:"nameofphone"`
+	Purchasedate string    `json:"purchasedate"`
+	Imeinumber   string    `json:"imeinumber"`
+	Model        string    `json:"model"`
+	Color        string    `json:"color"`
+	Description  string    `json:"description"`
+	CreatedAt    time.Time `json:"created_at"`
+	UpdatedAt    time.Time `json:"updated_at"`
+}
+
+type LaptopInsurance struct {
+	Id           int       `json:"id"`
+	Name         string    `json:"name"`
+	Email        string    `json:"email"`
+	Phonenumber  string    `json:"phonenumber"`
+	Nameofphone  string    `json:"nameofphone"`
+	Purchasedate string    `json:"purchasedate"`
+	Imeinumber   string    `json:"imeinumber"`
+	Model        string    `json:"model"`
+	Color        string    `json:"color"`
+	Description  string    `json:"description"`
+	CreatedAt    time.Time `json:"created_at"`
+	UpdatedAt    time.Time `json:"updated_at"`
+}
