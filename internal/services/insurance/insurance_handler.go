bf1df505125a80843f75d36399d91327719dfad6
jim-nnamdi <jimsamuel50@gmail.com>
2022-09-07 22:27:18 +0100

    (NewLaptopInsurance & mobileins additions) -> add new insurance for laptops

--- internal/services/insurance/insurance_handler.go
+++ internal/services/insurance/insurance_handler.go
@@ -30,6 +30,7 @@ func (ins *insurance) NewMobileInsurance(name string, email string, phonenumber 
 		return false, err
 	}
 	if !apply_for_new_mobile_insurance {
+		ins.logger.Debug("cannot add new mobile insurance application", zap.Bool("result", apply_for_new_mobile_insurance))
 		return false, errors.New(err.Error())
 	}
 	return true, nil
@@ -37,5 +38,14 @@ func (ins *insurance) NewMobileInsurance(name string, email string, phonenumber 
 
 // new laptop insurance
 func (ins *insurance) NewLaptopInsurance(name string, email string, phonenumber string, nameofphone string, purchasedate string, imeinumber string, model string, color string, description string, paid bool, createdAt time.Time, updatedAt time.Time) (bool, error) {
+	apply_for_new_laptop_insurance, err := ins.mysqlclient.ApplyForLaptopInsurance(name, email, phonenumber, nameofphone, purchasedate, imeinumber, model, color, description, paid, createdAt, updatedAt)
+	if err != nil {
+		ins.logger.Debug("error applying for laptop insurance", zap.String("error", err.Error()))
+		return false, err
+	}
+	if !apply_for_new_laptop_insurance {
+		ins.logger.Debug("cannot add new laptop insurance application", zap.Bool("result", apply_for_new_laptop_insurance))
+		return false, errors.New(err.Error())
+	}
 	return false, nil
 }
