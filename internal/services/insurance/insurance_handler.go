ad9bb10ac296f5aa65832e96532db18c7b111352
jim-nnamdi <jimsamuel50@gmail.com>
2022-09-07 22:23:31 +0100

    (NewMobileInsurance) -> add new insurance for mobile

--- internal/services/insurance/insurance_handler.go
+++ internal/services/insurance/insurance_handler.go
@@ -1,15 +1,38 @@
 package insurance
 
-import "time"
+import (
+	"errors"
+	"time"
+
+	"github.com/jim-nnamdi/kotts/internal/database"
+	"go.uber.org/zap"
+)
 
 var _ InsuranceInterface = &insurance{}
 
 type insurance struct {
+	logger      *zap.Logger
+	mysqlclient database.Client
+}
+
+func Newinsurance(logs *zap.Logger, db database.Client) *insurance {
+	return &insurance{
+		logger:      logs,
+		mysqlclient: db,
+	}
 }
 
 // new mobile insurance
 func (ins *insurance) NewMobileInsurance(name string, email string, phonenumber string, nameofphone string, purchasedate string, imeinumber string, model string, color string, description string, paid bool, createdAt time.Time, updatedAt time.Time) (bool, error) {
-	return false, nil
+	apply_for_new_mobile_insurance, err := ins.mysqlclient.ApplyForMobilePhoneInsurance(name, email, phonenumber, nameofphone, purchasedate, imeinumber, model, color, description, paid, createdAt, updatedAt)
+	if err != nil {
+		ins.logger.Debug("error applying for mobile insurance", zap.String("error", err.Error()))
+		return false, err
+	}
+	if !apply_for_new_mobile_insurance {
+		return false, errors.New(err.Error())
+	}
+	return true, nil
 }
 
 // new laptop insurance
